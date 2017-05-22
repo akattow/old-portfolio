61e288f77655a50443c37f510736b7f98d0ed70b
Kat Tow <kattow88@gmail.com>
2017-05-21 22:37:31 -0500

    Final update 5.21

---
+++ js/script.js
@@ -0,0 +1,52 @@
+//page scrolling
+  $("a[href^='#']").on("click", function(event) {
+    var target = $(this.getAttribute("href"));
+
+    if (target.length) {
+      event.preventDefault();
+      $("html, body").stop().animate({
+        scrollTop: target.offset().top
+      }, 500);
+    }
+   });
+
+//parallax images
+
+  var followX = 0,
+      followY = 0,
+      x = 0,
+      y = 0,
+      friction = 1 / 30,
+      moveBox;
+
+  function moveBackground() {
+    x += (followX - x) * friction;
+    y += (followY - y) * friction;
+
+    translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+
+    $(moveBox).css({
+      '-webit-transform': translate,
+      '-moz-transform': translate,
+      'transform': translate
+    });
+
+    window.requestAnimationFrame(moveBackground);
+  }
+
+  $(window).on('mousemove click', function(e) {
+
+    if (e.target.className !== 'image-box') return true;
+
+    moveBox = e.target;
+
+    var mouseX = Math.max(-100, Math.min(100, $(moveBox).offset().left + $(moveBox).width() / 2 - e.clientX));
+    var mouseY = Math.max(-100, Math.min(100, $(moveBox).position().top + $(moveBox).height() / 2 - e.clientY));
+    followX = (20 * mouseX) / 100; // 100 : 12 = mouxeX : follow
+    followY = (20 * mouseY) / 100;
+
+  });
+
+  moveBackground();
+
+
